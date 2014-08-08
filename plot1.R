cfe085110cd73b6bbf326f3890c7da75488a57a2
akelf <aneville@SERK.admin.wsd.ca>
2014-08-08 17:26:25 -0230

    Read data into dataframe

--- plot1.R
+++ plot1.R
@@ -1,1 +1,7 @@
-#
+## Project 1 Plot 1
+
+## Read the data file, restricting to just the dates 2007-02-01 to 2007-02-02
+library(sqldf)
+powerfile <- file("household_power_consumption.txt")
+powerdata <- sqldf("Select * from powerfile where Date='1/2/2007' or Date='2/2/2007'", file.format = list(sep=";",header = TRUE))
+
